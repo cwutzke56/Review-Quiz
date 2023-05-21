04d883c9a6fcebe7597215cd01c450e75932af28
cwutzke56 <calebwutzke56@gmail.com>
2023-05-21 13:16:14 -0500

    main start on code, create couple functions

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var timerEL = document.querySelector("#timer");
+
+function timerStart(){
+
+}
+
+function startQuestion(){
+
+}
+
+function questionClick(){
+
+}
+
+function endQuestion(){
+
+}
+
+function timerDown(){
+
+}
+
+function saveScore(){
+
+}
+
