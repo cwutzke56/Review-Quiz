69b807ba67402ff5e823cf67207498f932d9fbcf
cwutzke56 <calebwutzke56@gmail.com>
2023-05-24 22:11:14 -0500

    end questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,19 @@
 //declaring my DOM elements
-var timerEL = document.querySelector("#timer");
-var questionsEL = document.querySelector("#questions");
+var timerEl = document.querySelector("#timer");
+var questionsEl = document.querySelector("#questions");
 var choicesEl = document.querySelector("#options");
-var submitBtn = document.querySelector("#finalscore");
+var submitBtn = document.querySelector("#submitTest");
 var startBtn = document.querySelector("#start");
 var nameEl = document.querySelector("#name");
 var feedbackEl = document.querySelector("#feedback");
 var reStartBtn = document.querySelector("#restart");
-//questions being asked for quiz
-var questions = [
-    { prompt: "Inside which HTML element do we put the JavaScript?",
-    options: ["<javascript>", "<js>", "<script>", "<scripting>"],
-    answer: "<script>"
-},
 
-{
-    prompt: "How do you call a function named myFunction?",
-    options: ["call myFunction()", "myFunction()", "call function myFunction", "Call.myFunction"],
-    answer: "myFunction()"
-},
-
-{
-    prompt: "How does a for loop start?",
-    options: ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", " for (i <= 5; i++)"],
-    answer: "for (i = 0; i <= 5; i++)"
-},
-
-{
-    prompt: "In JavaScript, which of the following is a logical operator?",
-    options: ["|", "&&", "%", "/"],
-    answer: "&&" 
-},
-
-{
-    prompt: "A named element in a JavaScript program that is used to store and retrieve data is a _____.",
-    options: ["method", "assignment operator", "variable", "string"],
-    answer: "variable"
-}];
 
 //declaration of used variables throughout
 var time= questions.length*15;
 var timer;
-var questionCurrent;
+var currentQuestion;
+var finalQuestionIndex = questions.length;
 
 //clicking timer
 function timerStart(){
@@ -50,29 +22,50 @@ function timerStart(){
     if (time <= 0) {
       endQuestion();
 }}
+
+
+  
+
 //starts the test 
-function startQuestion(){
-    timerId = setInterval(clockTick, 1000);
+function startQuestions(){
+    timerId = setInterval(timerStart, 1000);
     timerEl.textContent = time;
-    var nextScreenEl = document.getElementById("start-screen");
+    var nextScreenEl = document.getElementById("startQuestion");
     nextScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
     getQuestion();
 }
-//
+startBtn.addEventListener("click", startQuestions);
+
+//questions being asked for quiz
+var questions = [
+
+];
+//goes through questionair
+function getQuestion() {
+   
+}
+//goes to next question and tells if right or wrong
 function questionClick(){
-
-}
-//
+   
+  }
+//ends the questions and timer and shows the final score
 function endQuestion(){
-
+    clearInterval(timerId);
+    var endScreenEl = document.getElementById("endQuestion");
+    endScreenEl.removeAttribute("class");
+    var finalScoreEl = document.getElementById("finalScore");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
 }
-//
-function timerDown(){
 
-}
-//
+//saves the score of test
 function saveScore(){
-
+ 
 }
+submitBtn.addEventListener("click", saveScore)
+
+
+
+
 
